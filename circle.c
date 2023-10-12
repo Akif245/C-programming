3c28a408573c08a3c952d5d441ee53631647307b
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-10-12 23:49:21 +0530

    area & perimeter of circle using Ubuntu

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int r;
+	float area,perimeter;
+	printf("Inter the value of r\n");
+	scanf("%d",&r);
+	area=3.14*r*r;
+	printf("area=%f\n",area);
+	perimeter=2*3.14*r;
+	printf("perimeter=%f\n",perimeter);
+}
