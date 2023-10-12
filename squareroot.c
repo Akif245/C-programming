72449eb8249979283fecd2d3f58aed8d52a83157
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-10-12 23:51:12 +0530

    sqrt(fcn) using Ubuntu

---
+++ squareroot.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main () 
+{ int a;
+  float result;
+  printf("Inter the value of a\n");
+  scanf("%d",&a);
+  result=sqrt(a);
+  printf("result =%f\n",result);
+}
